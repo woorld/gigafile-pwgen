d4d93c67c891868dc0056606d8ed08c04452570c
woorld <17719812+woorld@users.noreply.github.com>
2023-05-04 00:08:20 +0900

    パスワード設定ボタンの処理を追加

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+const randChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const passLength = 4;
 const target = document.getElementById('file_list');
 const obsConfig = {
   childList: true,
@@ -5,13 +7,28 @@ const obsConfig = {
   subtree: false,
 };
 const observer = new MutationObserver((mutationsList) => {
-  console.log('DOM Updated');
-  console.log(mutationsList);
+  // TODO: ボタンのスタイルを既存のものに合わせる処理の実装
+  const buttonStyle = getComputedStyle(document.getElementsByClassName('set_dlkey')[0]);
 
   for (mutation of mutationsList) {
     mutation.addedNodes.forEach((uploadFileArea) => {
       const button = document.createElement('button');
-      button.textContent = 'パスワード自動設定';
+
+      button.textContent = 'パスワード設定';
+      button.addEventListener('click', async () => {
+        const pass = generatePass();
+        // パスを入力して設定ボタンを押下
+        uploadFileArea.getElementsByClassName('dlkey_inp')[0].value = pass;
+        uploadFileArea.getElementsByClassName('set_dlkey gfbtn')[0].click();
+
+        try {
+          await navigator.clipboard.writeText(pass);
+        }
+        catch (e) {
+          alert('ダウンロードパスワードのクリップボードへのコピーに失敗しました: ' + e);
+        }
+      });
+
       uploadFileArea.getElementsByClassName('dlkey')[0].appendChild(button);
     });
   };
@@ -19,3 +36,7 @@ const observer = new MutationObserver((mutationsList) => {
 
 // #file_listを監視
 observer.observe(target, obsConfig);
+
+const generatePass = () => {
+  return Array.from(crypto.getRandomValues(new Uint32Array(passLength))).map((n) => randChar[n % randChar.length]).join('');
+};
