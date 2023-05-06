e723128d61fcbc3439130ad999a51528d8266b4b
woorld <17719812+woorld@users.noreply.github.com>
2023-05-06 21:03:51 +0900

    関数の宣言を移動
    ページ読み込み直後に関数を実行するとき、下で宣言していると実行できず処理が止まってしまうため

--- js/content.js
+++ js/content.js
@@ -1,10 +1,34 @@
+const randChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const pwLength = 4;
+const generatePw = () => {
+  return Array.from(crypto.getRandomValues(new Uint32Array(pwLength))).map((n) => randChar[n % randChar.length]).join('');
+};
+
+const copyComputedCssText = (target) => {
+  // スタイルの取得
+  const ignorePropName = /\d{1,}|width/;
+  const targetStyle = getComputedStyle(target);
+  let computedCssText = '';
+
+  // 取得したスタイルから必要なスタイルのみを抽出
+  for (const prop in targetStyle) {
+    const propVal = targetStyle.getPropertyValue(prop);
+    if (ignorePropName.test(prop) || propVal === '') {
+      console.log(prop, targetStyle.getPropertyValue(prop), false);
+      continue;
+    }
+    console.log(prop, targetStyle.getPropertyValue(prop), true);
+    computedCssText += `${prop}: ${propVal};`;
+  }
+
+  return computedCssText;
+}
+
 chrome.storage.sync.get(['isEnable'], (result) => {
   if (!result.isEnable) {
     return;
   }
 
-  const randChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  const pwLength = 4;
   const target = document.getElementById('file_list');
   const obsConfig = {
     childList: true,
@@ -17,7 +41,7 @@ chrome.storage.sync.get(['isEnable'], (result) => {
         const button = document.createElement('button');
 
         button.textContent = 'PW生成・設定';
-        button.style.cssText = copyComputedCssText(document.getElementsByClassName('set_dlkey')[0]);;
+        button.style.cssText = copyComputedCssText(document.getElementsByClassName('set_dlkey')[0]);
 
         button.addEventListener('click', async () => {
           const pw = generatePw();
@@ -43,26 +67,4 @@ chrome.storage.sync.get(['isEnable'], (result) => {
 
   // #file_listを監視
   observer.observe(target, obsConfig);
-
-  const generatePw = () => {
-    return Array.from(crypto.getRandomValues(new Uint32Array(pwLength))).map((n) => randChar[n % randChar.length]).join('');
-  };
-
-  const copyComputedCssText = (target) => {
-    // スタイルの取得
-    const ignorePropName = /\d{1,}|width/;
-    const targetStyle = getComputedStyle(target);
-    let computedCssText = '';
-
-    // 取得したスタイルから必要なスタイルのみを抽出
-    for (const prop in targetStyle) {
-      const propVal = targetStyle.getPropertyValue(prop);
-      if (ignorePropName.test(prop) || propVal === '') {
-        continue;
-      }
-      computedCssText += `${prop}: ${propVal};`;
-    }
-
-    return computedCssText;
-  }
 });
