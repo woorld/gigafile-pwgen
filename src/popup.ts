3ab4fb4add4a40a76799fe23816c24741bafc8a8
woorld <17719812+woorld@users.noreply.github.com>
2023-06-03 17:18:21 +0900

    #4の修正

--- src/popup.ts
+++ src/popup.ts
@@ -2,6 +2,12 @@ window.addEventListener('load', () => {
   const checkboxEnable = document.getElementById('enable') as HTMLInputElement;
   chrome.storage.sync.get(['isEnable'], (result) => {
     checkboxEnable.checked = result.isEnable;
+
+    // ポップアップ表示時にアニメーションするのを防ぐため、transitionを後付けする
+    // HACK: 直後にtransitionをオンにするとアニメーションしてしまうので、少し間を置く
+    setTimeout(() => {
+      document.getElementsByClassName('checkbox')[0].className = 'checkbox checkbox-initialized';
+    }, 50);
   });
 
   checkboxEnable.addEventListener('change', async (e) => {
