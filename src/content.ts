a1031ac018b2f8e23fada3b2773db42d299ac65d
woorld <17719812+woorld@users.noreply.github.com>
2023-06-03 13:22:01 +0900

    content.jsのTS対応

--- src/content.ts
+++ src/content.ts
@@ -1,23 +1,24 @@
+// TODO: 定数を切り分け
 const randChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 const pwLength = 4;
-const generatePw = () => {
+const generatePw = (): string => {
   return Array.from(crypto.getRandomValues(new Uint32Array(pwLength))).map((n) => randChar[n % randChar.length]).join('');
 };
 
-const copyComputedCssText = (target, ignoreProps) => {
+const copyComputedCssText = (target: HTMLElement, ignoreProps: Array<string>): string => {
   // スタイルの取得
-  const ignorePropName = ignoreProps !== undefined
+  const ignorePropName: RegExp | string = ignoreProps !== undefined
     ? new RegExp(`^(${ignoreProps.join('|')})$`)
     : '';
-  const targetStyle = getComputedStyle(target);
-  let computedCssText = '';
+  const targetStyle: CSSStyleDeclaration = getComputedStyle(target);
+  let computedCssText: string = '';
 
   // 取得したスタイルから必要なスタイルのみを抽出しCSSTextを組み立て
   for (let i = 0; i < targetStyle.length; i++) {
-    const propName = targetStyle.item(i);
-    const propVal = targetStyle.getPropertyValue(propName);
+    const propName: string = targetStyle.item(i);
+    const propVal: string = targetStyle.getPropertyValue(propName);
 
-    if ((ignorePropName !== '' && ignorePropName.test(propName)) || propVal === '') {
+    if ((ignorePropName !== '' && (ignorePropName as RegExp).test(propName)) || propVal === '') {
       continue;
     }
 
@@ -27,7 +28,7 @@ const copyComputedCssText = (target, ignoreProps) => {
   return computedCssText;
 }
 
-const copyToClipboard = async (copyText) => {
+const copyToClipboard = async (copyText: string): Promise<void> => {
   try {
     await navigator.clipboard.writeText(copyText);
   }
@@ -42,26 +43,26 @@ chrome.storage.sync.get(['isEnable'], (result) => {
   }
 
   // 「まとめる」ボタンの横に「パスワード付きでまとめる」ボタンを追加
-  const buttonPackUp = document.getElementById('matomete_btn');
+  const buttonPackUp: HTMLElement = document.getElementById('matomete_btn')!;
   const buttonPackUpWithPw = document.createElement('button');
 
   // 「まとめる」ボタンに合わせてspanでボタン内テキストを作成し追加
-  const buttonText = document.createElement('span');
+  const buttonText: HTMLElement = document.createElement('span');
   buttonText.textContent = 'パスワード付きでまとめる';
 
-  const buttonCssText = copyComputedCssText(buttonPackUp, ['width', 'inline-size', 'padding']) + 'padding: 5px;';
+  const buttonCssText: string = copyComputedCssText(buttonPackUp, ['width', 'inline-size', 'padding']) + 'padding: 5px;';
   buttonPackUpWithPw.style.cssText = buttonCssText;
   buttonPackUpWithPw.appendChild(buttonText);
 
   buttonPackUpWithPw.addEventListener('click', async () => {
-    const files = document.getElementsByClassName('file_info');
+    const files = document.getElementsByClassName('file_info')!;
 
     if (files.length <= 0) {
       alert('ファイルを選択してください。');
       return;
     }
 
-    for (file of files) {
+    for (const file of files) {
       const uploadStatus = file.getElementsByClassName('status')[0].textContent;
       const buttonCancelStatus = file.getElementsByClassName('cancel')[0].getAttribute('value');
 
@@ -72,48 +73,52 @@ chrome.storage.sync.get(['isEnable'], (result) => {
       return;
     }
 
-    const pw = generatePw();
+    const pw: string = generatePw();
     const obsConfig = {
       attributes: true,
     };
     const obsPackUp = new MutationObserver(async (mutationsList) => {
-      const dlUrl = mutationsList[0].target.attributes[4].value;
+      const dlUrl = (mutationsList[0].target as HTMLInputElement).attributes[4].value;
       const copyText = `${dlUrl}\nダウンロードパスワード：${pw}`;
 
       await copyToClipboard(copyText);
       obsPackUp.disconnect();
     });
 
-    obsPackUp.observe(document.getElementById('matomete_url'), obsConfig);
+    obsPackUp.observe(document.getElementById('matomete_url')!, obsConfig);
 
     // パスを入力して設定ボタンを押下
-    document.getElementById('zip_dlkey').value = pw;
+    (document.getElementById('zip_dlkey') as HTMLInputElement).value = pw;
     buttonPackUp.click();
   });
 
-  buttonPackUp.parentNode.appendChild(buttonPackUpWithPw);
+  buttonPackUp.parentNode!.appendChild(buttonPackUpWithPw);
 
-  const target = document.getElementById('file_list');
+  const target = document.getElementById('file_list')!;
   const obsConfig = {
     childList: true,
     characterData: false,
     subtree: false,
   };
   const observer = new MutationObserver((mutationsList) => {
-    for (mutation of mutationsList) {
+    for (const mutation of mutationsList) {
       mutation.addedNodes.forEach((uploadFileArea) => {
+        if (!(uploadFileArea instanceof Element)) {
+          return;
+        }
+
         const button = document.createElement('button');
 
         button.textContent = 'PW生成・設定';
-        button.style.cssText = copyComputedCssText(document.getElementsByClassName('set_dlkey')[0], ['width', 'inline-size']);
+        button.style.cssText = copyComputedCssText(document.getElementsByClassName('set_dlkey')[0] as HTMLButtonElement, ['width', 'inline-size']);
 
         button.addEventListener('click', async () => {
           const pw = generatePw();
           // パスを入力して設定ボタンを押下
-          uploadFileArea.getElementsByClassName('dlkey_inp')[0].value = pw;
-          uploadFileArea.getElementsByClassName('set_dlkey gfbtn')[0].click();
+          (uploadFileArea.getElementsByClassName('dlkey_inp')[0] as HTMLInputElement).value = pw;
+          (uploadFileArea.getElementsByClassName('set_dlkey gfbtn')[0] as HTMLElement).click();
 
-          const dlUrl = uploadFileArea.getElementsByClassName('file_info_url url')[0].value;
+          const dlUrl = (uploadFileArea.getElementsByClassName('file_info_url url')[0] as HTMLInputElement).value;
           const copyText = `${dlUrl}\nダウンロードパスワード：${pw}`;
 
           await copyToClipboard(copyText);
