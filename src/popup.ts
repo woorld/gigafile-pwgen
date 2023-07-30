7af2ea5f4e28a5f33f587ed60d1442e80648a48f
woorld <17719812+woorld@users.noreply.github.com>
2023-07-31 02:10:22 +0900

    複数設定項目の設定復元・反映に対応

--- src/popup.ts
+++ src/popup.ts
@@ -1,18 +1,23 @@
-window.addEventListener('load', () => {
-  const checkboxEnable = document.getElementById('enable') as HTMLInputElement;
-  chrome.storage.sync.get(['isEnable'], (result) => {
-    checkboxEnable.checked = result.isEnable;
+window.addEventListener('load', async () => {
+  const checkboxes = document.querySelectorAll('.input-row input[type="checkbox"]');
 
-    // ポップアップ表示時にアニメーションするのを防ぐため、transitionを後付けする
-    // HACK: 直後にtransitionをオンにするとアニメーションしてしまうので、少し間を置く
-    setTimeout(() => {
-      document.getElementsByClassName('checkbox')[0].className = 'checkbox checkbox-initialized';
-    }, 50);
+  await chrome.storage.sync.get(null, (result) => {
+    for (const checkbox of checkboxes) {
+      const storageValName = checkbox.id.replace(/-[a-z0-9]/g, str => str.slice(1).toUpperCase());
+
+      // 各チェックボックスのオンオフを設定値と同期させる
+      (checkbox as HTMLInputElement).checked = result[storageValName];
+
+      checkbox.addEventListener('change', async (e) => {
+        await chrome.storage.sync.set({ [storageValName]: (e.target as HTMLInputElement).checked })
+      });
+    }
   });
 
-  checkboxEnable.addEventListener('change', async (e) => {
-    await chrome.storage.sync.set({
-      isEnable: (e.target as HTMLInputElement).checked
-    });
-  })
+  // HACK: 直後にtransitionをオンにするとアニメーションしてしまうので、少し間を置く
+  setTimeout(() => {
+    for (const checkbox of checkboxes) {
+      checkbox.parentElement!.className = 'checkbox checkbox-initialized';
+    }
+  }, 50);
 });
